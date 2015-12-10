adc1027ed2fb1c547a9ece37ff332e8f745cdea9
Masaru Nagaku <mnagaku@gmail.com>
2015-12-10 15:58:17 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \
   --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" \
   >> /etc/apt/sources.list.d/docker.list
-apt-get update && apt-get install docker-engine
+apt-get update && apt-get install -y docker-engine
 service docker restart
 
 curl -L https://github.com/docker/compose/releases/download/\
