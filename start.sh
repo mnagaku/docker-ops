de009b26fc18e45804b963cd91b9aed3996de2fe
Masaru Nagaku <mnagaku@gmail.com>
2015-12-10 16:54:01 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,4 +11,7 @@ curl -L https://github.com/docker/compose/releases/download/\
 1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+mkdir jenkins
+chmod 777 jenkins
+
 docker-compose up -d
