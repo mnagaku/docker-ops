ccc4de16b8a8849bc70f7b80dd59c30068710e70
Masaru Nagaku <mnagaku@gmail.com>
2015-12-10 15:19:47 +0900

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \
+  --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" \
+  >> /etc/apt/sources.list.d/docker.list
+apt-get update && apt-get install docker-engine
+service docker restart
+
+curl -L https://github.com/docker/compose/releases/download/\
+1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose up -d
